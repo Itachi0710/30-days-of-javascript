a68c1f00d1b3fffbe52716b6056cfc8598a8c169
Aniket Shrivastava <101790241+Itachi0710@users.noreply.github.com>
2022-11-08 09:01:10 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Anket' // first name of a person
+let lastName = 'Shrivastava' // last name of a person
+let country = 'India' // country
+let city = 'Mumbai' // capital city
+let age = 19 // age in years
+let isMarried = falsw
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Virat Kohli', //name of a person
+  job = 'crickter',
+  live = 'India'
